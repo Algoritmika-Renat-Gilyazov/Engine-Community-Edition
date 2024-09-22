d88f594f98f304f11ce46333f87eb62c2f339dfc
Algoritmika-Renat-Gilyazov <road10mc@gmail.com>
2024-09-22 12:08:48 +0500

    script.py

---
+++ script.py
@@ -0,0 +1,6 @@
+import webbrowser
+new = 2
+url = "http://docs.python.org/library/webbrowser.html"
+webbrowser.open(url,new=new)
+url = "file:///C:/Users/renat/AppData/Local/ENGINE/index.html"
+webbrowser.open(url,new=new)
