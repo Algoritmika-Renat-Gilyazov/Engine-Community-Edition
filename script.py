00ff2d22ec2d7946ba56cbf6190e9880a6adc01f
Algoritmika-Renat-Gilyazov <road10mc@gmail.com>
2024-09-22 12:50:56 +0500

    Update script.py

--- script.py
+++ script.py
@@ -2,5 +2,5 @@ import webbrowser
 new = 2
 url = "http://docs.python.org/library/webbrowser.html"
 webbrowser.open(url,new=new)
-url = "file:///C:/Users/renat/AppData/Local/ENGINE/index.html"
+url = "file:///C:/ENGINE/index.html"
 webbrowser.open(url,new=new)
